8358863f87bdaaa39cc3264278a7be1801c8a45a
Alaggesan Palani <alagesann@gmail.com>
2016-07-31 15:06:37 +0530

    Rules Engine Ref

---
+++ api/src/main/java/org/openmrs/module/rulesengine/engine/RuleEngine.java
@@ -0,0 +1,10 @@
+package org.openmrs.module.rulesengine.engine;
+
+import org.openmrs.module.rulesengine.domain.Dose;
+
+/**
+ * Created by apalani on 7/31/16.
+ */
+public interface RuleEngine {
+    Dose calculateDose(String patientUUId, String drugName, String baseUnit, Double doseUnit, String orderSetName) throws Exception;
+}
---
+++ api/src/main/java/org/openmrs/module/rulesengine/engine/RulesEngineImpl.java
@@ -0,0 +1,15 @@
+package org.openmrs.module.rulesengine.engine;
+
+import org.openmrs.module.rulesengine.domain.Dose;
+
+/**
+ * Created by apalani on 7/31/16.
+ */
+public class RulesEngineImpl implements RuleEngine {
+
+    @Override
+    public Dose calculateDose(String patientUUId, String drugName, String baseUnit, Double doseUnit, String orderSetName) throws Exception {
+        return null;
+    }
+
+}
